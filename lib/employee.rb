d30deda708d3e61e48a6bd4b250bda5ce8b0d585
vagrant <victoria.duggan@shopify.com>
2017-02-14 22:42:15 +0000

    completed ActiveRecord validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,10 @@
 class Employee < ActiveRecord::Base
+validates :first_name, presence: true
+validates :last_name, presence: true
+validates :hourly_rate, numericality: {greater_than_or_equal_to: 40}
+validates :hourly_rate, numericality: {less_than_or_equal_to: 200}
+validates :store_id, presence: true
+
+
 belongs_to :store
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
+validates :name, length: { minimum: 3 }
+validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+# validate :carry_men_or_women
 has_many :employees
 end
+
+
+# def carry_men_or_women
+#   Store.each do |store|
+#   if store.mens_apparel || store.womens_apparel
+#     end
+#   end
+# end
