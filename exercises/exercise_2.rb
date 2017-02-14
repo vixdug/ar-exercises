550f53f3de46848b53dd04342d619a85b57c9b1a
vagrant <victoria.duggan@shopify.com>
2017-02-14 21:45:06 +0000

    extracting data via Active record

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,24 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+### Exercise 2: Update the first store
+#
+# 1. Load the first store (with `id = 1`) from the database and assign it to an instance variable `@store1`.
+# 2. Load the second store from the database and assign it to `@store2`.
+# 3. Update the first store (`@store1`) instance in the database. (Change its name or something.)
+#
+
+
+@store1 = Store.find(1);
+
+@store2 = Store.find(2);
+
+
+@store1.name = "vix"
+@store1.save
+
+p @store1
+
+# @store2 = Store.find(2)
+
+# puts "this is the answer #{@store1}"
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,34 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+# 2. Using the `where` class method from Active Record, fetch (a collection of) only those stores that carry men's apparel. Assign the results to a variable `@mens_stores`.
+# 3. Loop through each of these stores and output their name and annual revenue on each line.
+# 4. Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+
+Store.create(
+name: 'Surrey',
+annual_revenue: 224000,
+mens_apparel: false,
+womens_apparel: true
+ )
+
+Store.create(
+name: 'Whistler',
+annual_revenue: 430000,
+mens_apparel: true,
+womens_apparel: true
+)
+
+Store.create(
+name: 'Yaletown',
+annual_revenue: 430000,
+mens_apparel: true,
+womens_apparel: true
+ )
+
+ @mens_stores = Store.where({mens_apparel: true})
+
+ @mens_stores.each do |store|
+   p "#{store.name}  revenue is: #{store.annual_revenue}"
+ end
