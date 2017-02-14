4c3bdd56880db08ee1789b0f45ad1ddfda5e1103
vagrant <victoria.duggan@shopify.com>
2017-02-14 22:12:19 +0000

    completed foreign key association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+### Exercise 6: One-to-many association
+#
+
+
+
+@store1.employees.create(first_name: "vix", last_name: "dug", hourly_rate: 600)
+
+@store1.employees.create(first_name: "nathe", last_name: "bowes", hourly_rate: 6)
+
+@store2.employees.create(first_name: "bob", last_name: "cat", hourly_rate: 68)
+
+@store2.employees.create(first_name: "plant", last_name: "er", hourly_rate: 90)
